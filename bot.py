0b055c9418d190d76de2268de965166e5d2c4227
Galachard <kkrzys2004@gmail.com>
2021-07-29 22:02:07 +0200

    Added more functions to functions.txt, developed help command, bugfixes

--- bot.py
+++ bot.py
@@ -53,7 +53,22 @@ async def on_message(message):
         else:   # Help with specific commands
             message.content = message.content[len(help_com) + 1:]
             if message.content == plot_text:
-                await message.channel.send('This is supposed to be helpful')
+                await message.channel.send('Plots the graph of a given function.'
+                                           'Example: ' + plot_com + '2 * x^2 - 4 * x + 7')
+            if message.content.startswith(calc_text):
+                if message.content == calc_text:
+                    await message.channel.send('Calculates the value of a given mathematical expression.\n' +
+                                               'Example: ' + calc_com + '7 ^ 3 - 3! + sin(0)\n' +
+                                               'Help for a specific function: ' + help_com + calc_text +
+                                               'function (e.g. gcd)')
+                else:
+                    message.content = message.content[len(calc_text) + 1:]
+                    for index in range(len(calculating.functions)):
+                        f = calculating.functions[index].get_help(message.content)
+                        if f is not None:
+                            await message.channel.send(f[0] + ': ' + f[1] + '\n' +
+                                                       'Example: ' + f[2][0] + ' ---> ' + f[2][1])
+                            break
 
     if message.content.startswith('%hello'):
         msg = 'Hello {0.author.mention}'.format(message)
@@ -84,6 +99,8 @@ async def on_message(message):
             await message.channel.send('Calculate command: Incorrect input!')
         except OverflowError:
             await message.channel.send('Calculate command: Calculated number is too big!')
+        except ZeroDivisionError:
+            await message.channel.send('Calculate command: Division by zero detected!')
         except ArithmeticError:
             await message.channel.send('Calculate command: Incorrect amount of arguments of a function!')
 
